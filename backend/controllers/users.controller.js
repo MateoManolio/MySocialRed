bb30608b2960324ea7e3f4def5070da95b543a8f
babybluec10h15n <gdicosimo@alumnos.exa.unicen.edu.ar>
2023-06-02 11:44:10 -0300

    Correccion de del insertado en la base 2.0

--- backend/controllers/users.controller.js
+++ backend/controllers/users.controller.js
@@ -14,17 +14,17 @@ const getAllUsers = (req, res) => {
 const createUser = (req, res) => {
     // curl -X POST -H "Content-Type: application/json" -d '{"username": "42944803", "psw": 123, "name":"gonza" , "surname":"dicosimo", "study" : "Ing. de Sistemas", "profession" : "MMO" }' http://localhost:3000/users
 
-    searchDB(MODEL, JSON.parse(req.body) )
+    searchDB(MODEL, {"username":req.body.username})
     .then((element) => {
         if (element && element.length > 0) {
             res.status(400).send({
                 status: "error",
-                message: req.body.firstName + " is already exist"
+                message: req.body.username + " is already exist"
             })        
         } else {
             insertDB(MODEL, req.body)
             .then( (element) => {
-                res.status(201).json({ message: "New contact created!", data: element})
+                res.status(201).json({ message: "New user created!", data: element})
             })
             .catch ( (error) => {
                 return res.status(400).json({status:"error", error: error});
@@ -53,7 +53,27 @@ const updateUser = (req, res) => {
 };
 
 const deleteUser = (req, res) => {
-
+    // Tengo en cuenta que el username es el id
+    searchDB(MODEL, {"username":req.body.username})
+    .then((element) => {
+        if (element && element.length > 0) {
+            insertDB(MODEL, req.body)
+            .then((element) => {
+                res.status(201).json({ message: "New user created!", data: element})
+            })
+            .catch ((error) => {
+                return res.status(400).json({status:"error", error: error});
+            })
+        } else {
+            res.status(400).send({
+                status: "error",
+                message: req.body.username + "does not exist"
+            })    
+        }
+    })
+    .catch((error) => {
+        return res.status(400).json({status:"error", error: error});
+    })
 };
 
 export { getAllUsers, createUser, getUserById, updateUser, deleteUser };
